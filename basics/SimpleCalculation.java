797f6d32406cf06147f2e9fb9597789cf45df68f
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-02-04 19:28:51 -0500

    SimpleCalculation.java

---
+++ basics/SimpleCalculation.java
@@ -0,0 +1,12 @@
+public class SimpleCalculation {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+
+    }
+}
