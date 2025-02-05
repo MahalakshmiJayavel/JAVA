c473107faeaee9681681941ebb6ffc95b742c501
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-02-04 20:00:16 -0500

    CommentPractice.java

---
+++ basics/CommentPractice.java
@@ -0,0 +1,22 @@
+// Define class named SimpleCalculation
+public class SimpleCalculation {
+    // Main method where the program execution begins
+    public static void main(String[] args) {
+        // Declare and initialize an integer variable 'a' with a value of 15
+        int a = 15;
+        // Declare and initialize an integer variable 'b' with a value of 20
+        int b = 20;
+        
+        // Perform addition of 'a' and 'b', and print the result
+        System.out.println(a + b);  // Output is 35
+        
+        // Perform subtraction of 'b' from 'a', and print result
+        System.out.println(a - b);  // Output is -5
+        
+        // Perform division of 'a' by 'b', and print the result
+        System.out.println(a / b);  // Output is 0 
+        
+        // Perform multiplication of 'a' and 'b', and print the result
+        System.out.println(a * b);  // Output is 300
+    }
+}
